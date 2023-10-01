b1f5b89f52c7b1dd4092ddf2a1e5d5eac89723b0
Sherman Burwell <74177092+shermanburwell3@users.noreply.github.com>
2023-10-01 03:06:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare('passw0rd!', hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 
 
 //END_ASYNC
