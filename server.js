cb20d5cdfd5b6131467d83b005e74d1dd64a56bb
Sherman Burwell <74177092+shermanburwell3@users.noreply.github.com>
2023-10-01 03:14:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
